2de18808276cf6fe03e3d6f8e0b8712e358030bd
Alexander <alexxx547@gmail.com>
2019-05-22 21:29:11 +0300

    Updates

---
+++ precache-manifest.67666b1fdedfd40a221df456a5712912.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34c19772ab32cbb3b83494b388f96a1b",
+    "url": "/re-store/index.html"
+  },
+  {
+    "revision": "7f7495dc07af40994d88",
+    "url": "/re-store/static/css/main.55bb638b.chunk.css"
+  },
+  {
+    "revision": "a52b469f6355ab71ab82",
+    "url": "/re-store/static/js/2.4d3b7d6b.chunk.js"
+  },
+  {
+    "revision": "7f7495dc07af40994d88",
+    "url": "/re-store/static/js/main.c2b81348.chunk.js"
+  },
+  {
+    "revision": "8747fd79f7d838edaecc",
+    "url": "/re-store/static/js/runtime~main.508d4c4e.js"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/re-store/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "/re-store/static/media/Roboto.69eee0b4.woff2"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/re-store/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "15a913e1228cca9b1247b373bdf27e55",
+    "url": "/re-store/static/media/Robotobold.15a913e1.woff2"
+  },
+  {
+    "revision": "1fc6a83d1e366dfe7babb690d33ba6fb",
+    "url": "/re-store/static/media/Robotobold.1fc6a83d.ttf"
+  },
+  {
+    "revision": "7bf74824d7a8b3c08fe5956f93e9ce2a",
+    "url": "/re-store/static/media/Robotobold.7bf74824.woff"
+  },
+  {
+    "revision": "38e1ed9a9ee49d755d20226b0d66588c",
+    "url": "/re-store/static/media/Robotolight.38e1ed9a.woff2"
+  },
+  {
+    "revision": "611ac98371c2d77c377041e1a0fd87dc",
+    "url": "/re-store/static/media/Robotolight.611ac983.woff"
+  },
+  {
+    "revision": "63d6f579b0db1fc75cc977a0539ce45f",
+    "url": "/re-store/static/media/Robotolight.63d6f579.ttf"
+  },
+  {
+    "revision": "38aa81d461c299fa9f0cb27726401996",
+    "url": "/re-store/static/media/Robotomedium.38aa81d4.ttf"
+  },
+  {
+    "revision": "4a34361b26e9896ea1b5065708f8daea",
+    "url": "/re-store/static/media/Robotomedium.4a34361b.woff2"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/re-store/static/media/Robotomedium.6422eac7.woff"
+  }
+]);
